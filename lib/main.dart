bd091615a45ec2bff81c760a14baa8f6b9d29049
Danish Bansal <danishbansal60@gmail.com>
2025-05-16 19:08:57 +0530

    added the skeleton for the sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,146 @@ import 'package:tikitar_demo/features/webview/meeting_list_screen.dart';
 import 'package:tikitar_demo/features/webview/my_profile_screen.dart';
 import 'package:tikitar_demo/features/webview/task_screen.dart';
 
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final _databaseName = "tikitar.db";
+  static final _databaseVersion = 1;
+
+  static final table = 'locations';
+  static final columnId = '_id';
+  static final columnName = 'name';
+  static final columnLatitude = 'latitude';
+  static final columnLongitude = 'longitude';
+
+  // make this a singleton class
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  // only have a single app-wide reference to the database
+  static Database? _database;
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  // this opens the database (and creates it if it doesn't exist)
+  _initDatabase() async {
+    String path = join(await getDatabasesPath(), _databaseName);
+    return await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE $table (
+            $columnId INTEGER PRIMARY KEY,
+            $columnName TEXT NOT NULL,
+            $columnLatitude TEXT NOT NULL,
+            $columnLongitude TEXT NOT NULL
+          )
+          ''');
+      },
+    );
+  }
+
+  // insert
+  Future<int> insertData(Map<String, dynamic> rowData) async {
+    Database db = await database;
+    return await db.insert(table, rowData);
+  }
+
+  // retreive all
+  Future<List<Map<String, dynamic>>> retrieveAllData() async {
+    Database db = await database;
+    return await db.query(table);
+  }
+
+  // UPDATE
+  Future<int> updateData(Map<String, dynamic> rowData) async {
+    // here we need to add the
+    Database db = await database;
+    int id = rowData[columnId];
+    return await db.update(
+      table,
+      rowData,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // DELETE
+  Future<int> deleteData(int id) async {
+    Database db = await database;
+    return await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure this line is there
-  //You're calling SharedPreferences.getInstance() before Flutter is fully ready, 
+  //You're calling SharedPreferences.getInstance() before Flutter is fully ready,
   //most likely before WidgetsFlutterBinding.ensureInitialized() is called.
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+  DatabaseHelper dbHelper = DatabaseHelper._privateConstructor();
+  List<Map<String, dynamic>> _locations = [];
+
+  
+  @override
+  void initState() {
+    super.initState();
+    _performDatabaseOperations();
+  }
+
+  Future<void> _performDatabaseOperations() async {
+    // Insert sample data
+    int insertedId = await dbHelper.insertData({
+      'name': 'Sample Place',
+      'latitude': '28.7041',
+      'longitude': '77.1025',
+    });
+    print('Inserted ID: $insertedId');
+
+    // Retrieve all rows
+    List<Map<String, dynamic>> allRows = await dbHelper.retrieveAllData();
+    print('All rows: $allRows');
+
+    // Update the inserted row
+    await dbHelper.updateData({
+      '_id': insertedId,
+      'name': 'Updated Place',
+      'latitude': '28.7000',
+      'longitude': '77.1000',
+    });
+
+    // Retrieve again
+    List<Map<String, dynamic>> updatedRows = await dbHelper.retrieveAllData();
+    print('After update: $updatedRows');
+
+    // Delete the row
+    await dbHelper.deleteData(insertedId);
+
+    // Check final state
+    List<Map<String, dynamic>> finalRows = await dbHelper.retrieveAllData();
+    print('After delete: $finalRows');
+
+    // Update UI with current DB data
+    setState(() {
+      _locations = finalRows;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,9 @@ import file_picker
 import file_selector_macos
 import flutter_inappwebview_macos
 import geolocator_apple
+import path_provider_foundation
 import shared_preferences_foundation
+import sqflite_darwin
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -17,6 +19,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
