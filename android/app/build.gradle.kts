cb8d2fdfada19ff7bb0e7b363504f54e6d8e69db
Danish Bansal <danishbansal60@gmail.com>
2025-07-08 23:54:46 +0530

    updated the targetSdk in app/build.gradle.kts to 34

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
-        targetSdk = 33
-        versionCode = flutter.versionCode
+        targetSdk = 34
+        versionCode = 3
         versionName = flutter.versionName
     }
 
